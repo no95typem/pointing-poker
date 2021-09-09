9558a9eebb21d381a9d343b5d12e06fd37894f8f
no95typem <no95typem@gmail.com>
2021-09-09 15:37:59 +0400

    fix: fix connection status toast

--- client/src/locales/locale-us.ts
+++ client/src/locales/locale-us.ts
@@ -4,6 +4,10 @@ import { LocaleKey } from './locale';
 const LocaleUS: Record<LocaleKey, string> = {
   SESSION_DEALER_KICK_CONFIRMATION_POPUP_BODY:
     'Do you really want to kick this player?',
+
+  APP_SERVER_CONNECTION_STATUS_TOAST_TITLE: 'Connection info',
+
+  APP_SERVER_CONNECTION_STATUS_TOAST_DESC: 'Succesfully connected to a server',
 };
 
 export const LOCALE_US = OBJ_PROCESSOR.deepFreeze(LocaleUS);
