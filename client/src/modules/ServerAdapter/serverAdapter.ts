dea8ab0f6a62453d73ee2ece71ccfdfbff16bd3e
no95typem <no95typem@gmail.com>
2021-09-28 20:10:03 +0400

    fix: fix some things after merging

--- client/src/modules/ServerAdapter/serverAdapter.ts
+++ client/src/modules/ServerAdapter/serverAdapter.ts
@@ -395,7 +395,7 @@ class ServerAdapter {
   };
 
   startGame = () => {
-    const settings = store.getState().settings;
+    const settings = OBJ_PROCESSOR.deepClone(store.getState().settings);
     this.updSessState({ gSettings: settings });
     store.dispatch(
       setGLoadByKey({
